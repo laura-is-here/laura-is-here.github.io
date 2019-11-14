7d8c2296b8dfc009b12e0e3b424e9f90bbe6746d
Laura Campa <lauras_here@pm.me>
2019-11-14 08:58:55 -0800

    rebuilding site Thu 14 Nov 2019 08:58:55 AM PST

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,12 +3,37 @@ const html = document.querySelector('html');
 const menuContent = document.querySelector('.nav-content');
 const headerHeight = document.querySelector('header').scrollHeight;
 const navBar = document.querySelector('nav');
-const menuToggleIcon = menuToggle.children;
 
 document.addEventListener('DOMContentLoaded', () => {
     html.classList.toggle('js');
 });
 
+var themeSwitch = document.getElementById('themeSwitch');
+if(themeSwitch) {
+  initTheme(); // on page load, if user has already selected a specific theme -> apply it
+
+  themeSwitch.addEventListener('change', function(event){
+    resetTheme(); // update color theme
+  });
+
+  function initTheme() {
+    var darkThemeSelected = (localStorage.getItem('themeSwitch') !== null && localStorage.getItem('themeSwitch') === 'dark'); 
+    // update checkbox
+    themeSwitch.checked = darkThemeSelected;
+    // update body data-theme attribute
+    darkThemeSelected ? document.body.setAttribute('data-theme', 'dark') : document.body.removeAttribute('data-theme');
+  };
+
+  function resetTheme() {
+    if(themeSwitch.checked) { // dark theme has been selected
+      document.body.setAttribute('data-theme', 'dark');
+      localStorage.setItem('themeSwitch', 'dark'); // save theme selection 
+    } else {
+      document.body.removeAttribute('data-theme');
+      localStorage.removeItem('themeSwitch'); // reset theme selection 
+    } 
+  };
+}
 /*
 window.onscroll = function() {
     if ((document.body.scrollTop > headerHeight || document.documentElement.scrollTop > headerHeight) && (!menuContent.classList.contains('show'))) {
