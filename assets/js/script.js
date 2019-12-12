fc23700a66945d0610892f9dc6d22fef98d01587
Laura Campa <lauras_here@pm.me>
2019-12-11 16:27:12 -0800

    rebuilding site Wed 11 Dec 2019 04:27:12 PM PST

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,27 +4,59 @@ const menuContent = document.querySelector('.nav-content');
 const navBar = document.querySelector('nav');
 
 document.addEventListener('DOMContentLoaded', () => {
-    html.classList.toggle('js');
+  html.classList.toggle('js');
 });
 
-var themeSwitch = document.getElementById('themeSwitch');
+const toggle = document.querySelector('[aria-pressed]');
+
+if(toggle) {
+  initTheme();
+  
+  toggle.addEventListener('click', (e) => {  
+    let pressed = e.target.getAttribute('aria-pressed') === 'true';
+    e.target.setAttribute('aria-pressed', String(!pressed));
+    resetTheme(e, pressed);
+  });
+  
+  function initTheme() {
+    var darkThemeSelected = (localStorage.getItem('themeSwitch') !== null && localStorage.getItem('themeSwitch') === 'dark'); 
+    
+    toggle.setAttribute('aria-pressed', darkThemeSelected);
+    
+    darkThemeSelected ? document.body.setAttribute('data-theme', 'dark') : document.body.removeAttribute('data-theme');
+  }
+  
+  function resetTheme(e, pressed) {
+    if (pressed !== true) {
+      document.body.setAttribute('data-theme', 'dark'); 
+      localStorage.setItem('themeSwitch', 'dark');  
+    } else {
+      document.body.removeAttribute('data-theme');
+      localStorage.removeItem('theme-switch');
+    }
+  } 
+}
+
+/*
+
+const themeSwitch = document.querySelector('#themeSwitch');
 if(themeSwitch) {
   initTheme(); // on page load, if user has already selected a specific theme -> apply it
-
+  
   themeSwitch.addEventListener('change', function(event){
     resetTheme(); // update color theme
   });
-
+  
   function initTheme() {
     var darkThemeSelected = (localStorage.getItem('themeSwitch') !== null && localStorage.getItem('themeSwitch') === 'dark'); 
     // update checkbox
-    themeSwitch.checked = darkThemeSelected;
+    themeSwitch.setAttribute('aria-pressed', 'true');
     // update body data-theme attribute
     darkThemeSelected ? document.body.setAttribute('data-theme', 'dark') : document.body.removeAttribute('data-theme');
   };
-
+  
   function resetTheme() {
-    if(themeSwitch.checked) { // dark theme has been selected
+    if(themeSwitch.getAttribute('aria-pressed', 'true')) { // dark theme has been selected
       document.body.setAttribute('data-theme', 'dark');
       localStorage.setItem('themeSwitch', 'dark'); // save theme selection 
     } else {
@@ -33,25 +65,22 @@ if(themeSwitch) {
     } 
   };
 }
-/*
 window.onscroll = function() {
-    if ((document.body.scrollTop > headerHeight || document.documentElement.scrollTop > headerHeight) && (!menuContent.classList.contains('show'))) {
-        navBar.classList.add('shrink');
-    } else if (navBar.classList.contains('shrink')) {
-        navBar.classList.remove('shrink');
-    }
+  if ((document.body.scrollTop > headerHeight || document.documentElement.scrollTop > headerHeight) && (!menuContent.classList.contains('show'))) {
+    navBar.classList.add('shrink');
+  } else if (navBar.classList.contains('shrink')) {
+    navBar.classList.remove('shrink');
+  }
 }
-*/
 
-/*
 menuToggle.addEventListener('click', () => {
-   menuContent.classList.toggle('show'); 
-   if (menuContent.classList.contains('show')) {
-       menuToggleIcon.item(0).innerHTML = 'Close';
-       menuToggleIcon.item(1).classList.replace('fa-bars', 'fa-window-close');
-   } else {
-       menuToggleIcon.item(0).innerHTML = 'Menu';
-       menuToggleIcon.item(1).classList.replace('fa-window-close', 'fa-bars');
-   }
+  menuContent.classList.toggle('show'); 
+  if (menuContent.classList.contains('show')) {
+    menuToggleIcon.item(0).innerHTML = 'Close';
+    menuToggleIcon.item(1).classList.replace('fa-bars', 'fa-window-close');
+  } else {
+    menuToggleIcon.item(0).innerHTML = 'Menu';
+    menuToggleIcon.item(1).classList.replace('fa-window-close', 'fa-bars');
+  }
 });
 */
