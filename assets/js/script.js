57dda43d765b493501d4ce80951dd4abf8646e47
Laura Campa <lauras_here@pm.me>
2019-12-13 15:34:50 -0800

    rebuilding site Fri 13 Dec 2019 03:34:50 PM PST

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,6 +22,12 @@ if(toggle) {
     let mediaQueryList = window.matchMedia('(prefers-dark-theme)');
     let darkThemeSelected = ((localStorage.getItem('themeSwitch') !== null && localStorage.getItem('themeSwitch') === 'dark') || (mediaQueryList.matches)); 
     
+    let colorThemeSelected = localStorage.getItem('colorSwitch');
+    if(colorThemeSelected) {
+      setColor(colorThemeSelected);
+    }
+    console.log(colorThemeSelected);
+    
     toggle.setAttribute('aria-pressed', darkThemeSelected);
     
     darkThemeSelected ? document.body.setAttribute('data-theme', 'dark') : document.body.removeAttribute('data-theme');
@@ -33,57 +39,66 @@ if(toggle) {
       document.body.setAttribute('data-theme', 'dark'); 
       window.setTimeout(function() {
         document.documentElement.classList.remove('color-in-transition') }, 1000
-      );
-      localStorage.setItem('themeSwitch', 'dark');  
-    } else {
-      document.documentElement.classList.add('color-in-transition');
-      document.body.removeAttribute('data-theme');
-      window.setTimeout(function() {
-        document.documentElement.classList.remove('color-in-transition') }, 1000
-      );
-      localStorage.removeItem('themeSwitch');
+        );
+        localStorage.setItem('themeSwitch', 'dark');  
+      } else {
+        document.documentElement.classList.add('color-in-transition');
+        document.body.removeAttribute('data-theme');
+        window.setTimeout(function() {
+          document.documentElement.classList.remove('color-in-transition') }, 1000
+          );
+          localStorage.removeItem('themeSwitch');
+        }
+      }; 
     }
-  }; 
-}
-
-const colorSwitch = document.querySelector('#colorSwitch');
-
-if(colorSwitch) {
-  colorSwitch.addEventListener('click', (e) => {
-    let target = e.target.nextElementSibling;
-
-    if(target.hasAttribute('hidden')) {
-      target.removeAttribute('hidden');
-      e.target.setAttribute('aria-expanded', 'true');
-      target.addEventListener('click', (e) => changeColor(e))
-    } else {
-      target.setAttribute('hidden', '');
-      e.target.setAttribute('aria-expanded', 'false');
+    
+    function setColor(colorThemeSelected) {
+      localStorage.setItem('colorSwitch', colorThemeSelected);  
+      document.documentElement.style.setProperty('--main-hue', ['var(--' + colorThemeSelected + ')']);
     }
-  });
-
-  function changeColor(e) {
-    if (e.target.classList.contains('orange-hue')) {
-      document.documentElement.classList.add('color-in-transition');
-      document.documentElement.style.setProperty('--main-hue', 'var(--red-hue');
-      document.documentElement.style.setProperty('--accent-hue', 'var(--yellow-hue');
-      window.setTimeout(function() {
-        document.documentElement.classList.remove('color-in-transition') }, 1000
-      );
-    } else if (e.target.classList.contains('blue-hue')) {
-      document.documentElement.classList.add('color-in-transition');
-      document.documentElement.style.setProperty('--main-hue', 'var(--blue-hue');
-      document.documentElement.style.setProperty('--accent-hue', 'var(--pink-hue');
-      window.setTimeout(function() {
-        document.documentElement.classList.remove('color-in-transition') }, 1000
-      );
-    } else if (e.target.classList.contains('pink-hue')) {
-      document.documentElement.classList.add('color-in-transition');
-      document.documentElement.style.setProperty('--main-hue', 'var(--pink-hue');
-      document.documentElement.style.setProperty('--accent-hue', 'var(--green-hue');
-      window.setTimeout(function() {
-        document.documentElement.classList.remove('color-in-transition') }, 1000
-      );
-    }
-  }
-}
+    
+    const colorSwitch = document.querySelector('#colorSwitch');
+    
+    if(colorSwitch) {
+      colorSwitch.addEventListener('click', (e) => {
+        let target = e.target.nextElementSibling;
+        
+        document.querySelector('.close').addEventListener('click', () => {
+          target.setAttribute('hidden', '');
+          colorSwitch.setAttribute('aria-expanded', 'false');
+        });
+        
+        if(target.hasAttribute('hidden')) {
+          target.removeAttribute('hidden');
+          e.target.setAttribute('aria-expanded', 'true');
+          target.addEventListener('click', (e) => {
+            changeColor(e);
+          });
+        } else {
+          target.setAttribute('hidden', '');
+          e.target.setAttribute('aria-expanded', 'false');
+        }
+      });
+      
+      function close(e, target) {
+        if(e.target.classList.contains('close')) {
+        }
+      }
+      
+      function changeColor(e) {
+        document.documentElement.classList.add('color-in-transition');
+        if (e.target.classList.contains('orange-hue')) {
+          colorThemeSelected = 'orange-hue';
+          setColor(colorThemeSelected);
+        } else if (e.target.classList.contains('blue-hue')) {
+          colorThemeSelected = 'blue-hue';
+          setColor(colorThemeSelected);
+        } else if (e.target.classList.contains('pink-hue')) {
+          colorThemeSelected = 'pink-hue';
+          setColor(colorThemeSelected);
+        }
+        window.setTimeout(function() {
+          document.documentElement.classList.remove('color-in-transition') }, 1000
+          );
+        }
+      }
